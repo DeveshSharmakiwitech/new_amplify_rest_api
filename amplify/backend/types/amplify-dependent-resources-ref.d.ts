318ace01e0e7ad0573f34ae0ee0cf3122d1c29f9
DeveshSharmakiwitech <devesh.sharma@kiwitech.com>
2023-05-11 11:58:53 +0530

    commit all files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,41 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsappapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "awsappapide90ac11": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "awsapifunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "awsapidb": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Amplify, API } from 'aws-amplify';
+
+import React, { useEffect, useState } from 'react';
+import { Authenticator, Button } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 function App() {
+  const [awsappName, setawsappName] = useState('')
+  const [awsappEmail, setawsappEmail] = useState('')
+  const [awsappAddress, setawsappAddress] = useState('')
+
+  useEffect(()=>{
+    API.get('awsappapi', '/awsapp/email').then((awsappRes) => console.log('awsappRes >> ', awsappRes)).catch((err)=> console.log('err :>> ', err)); 
+  },[])
+
+  const handelSubmit = e =>{
+    e.preventDefault()
+    API.put('awsappapi', '/awsapp', {
+      body: {
+        name: awsappName,
+        email: awsappEmail,
+        address: awsappAddress
+      }
+    }).then( resData => console.log('resData :>> ', resData))
+    .catch(err=>console.log('errRes :>> ', err));
+  }
+
+  const handelSubmit1 = e => {
+    e.preventDefault()
+    API.del('awsappapi', '/awsapp?name='+awsappName, {
+      // body: {
+      //   email: awsappEmail,
+      // }
+    }).then( resData => console.log('Delete resData :>> ', resData))
+    .catch(err=>console.log('errRes :>> ', err));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+          <Authenticator>
+      {({ signOut, user }) => {
+        return (
+        <main>
+          <form onSubmit={(event) =>handelSubmit(event)}>
+            <input value={awsappName} placeholder='name' onChange={(e)=>setawsappName(e.target.value)}/>
+            <input value={awsappEmail} placeholder='email' onChange={(e)=>setawsappEmail(e.target.value)}/>
+            <input value={awsappAddress} placeholder='address' onChange={(e)=>setawsappAddress(e.target.value)}/>
+            <Button style={{backgroundColor: '#fff'}} type='submit'>Add</Button>
+          </form>
+          <form onSubmit={(event) =>handelSubmit1(event)}>
+          <input value={awsappName} placeholder='name' onChange={(e)=>setawsappName(e.target.value)}/>
+            <Button style={{backgroundColor: '#fff'}} type='submit'>Delete</Button>
+          </form>
+          <h1>Hello {user.attributes.email}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}}
+    </Authenticator>
       </header>
     </div>
-  );
+
+  )
 }
 
 export default App;
